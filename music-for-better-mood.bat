21f6d124fe894d3ed4a470b771c13d2480706a02
ToxicDEV <95045254+T0xicDEV@users.noreply.github.com>
2022-01-28 02:29:50 +0100

    Add files via upload

---
+++ music-for-better-mood.bat
@@ -0,0 +1,10 @@
+@echo off
+start https://youtu.be/W3q8Od5qJio?t=30
+start https://youtu.be/CYiGyaJyPMk?t=14
+start https://www.youtube.com/watch?v=gaMq-D7K8ls
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ&t
+start https://www.youtube.com/watch?v=cB4TUza4Fs0
+start https://www.youtube.com/watch?v=Sf-sRscz7bY
+start https://www.youtube.com/watch?v=-vyHIzlzlLQ
+start https://www.youtube.com/watch?v=klr7-UxVino
+start https://www.youtube.com/watch?v=P719ZYkrA3A
